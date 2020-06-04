b86594dbd91a91909997fe4fc1be7d4b7d561086
Klarys <44509197+Klarys@users.noreply.github.com>
2020-06-04 18:54:30 +0200

    added json parsing

--- project/node-rest-api/api/routes/basic.js
+++ project/node-rest-api/api/routes/basic.js
@@ -7,16 +7,21 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'post endpoint works!'
-    });
-});
-
 router.get('/:id', (req, res, next) => {
     res.status(200).json({
         message: "requested get for parameter " + req.params.id
     });
+});
+
+router.post('/', (req, res, next) => {
+    const basicObject = {
+        id: req.body.id,
+        prop: req.body.prop
+    }
+    res.status(201).json({
+        message: "received json object with properties id: " + basicObject.id + " and prop: " + basicObject.prop,
+        createdObject: basicObject
+    })
 });
 
 module.exports = router;
--- project/node-rest-api/app.js
+++ project/node-rest-api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 const basicRoutes = require('./api/routes/basic');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/basic', basicRoutes);
 
