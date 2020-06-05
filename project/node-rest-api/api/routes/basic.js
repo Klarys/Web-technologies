9075adceb4d2ff33906ada4291c5c044e18349fb
Klarys <44509197+Klarys@users.noreply.github.com>
2020-06-05 15:20:13 +0200

    Added basic mysql connections in the API

--- project/node-rest-api/api/routes/basic.js
+++ project/node-rest-api/api/routes/basic.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mysql = require('mysql');
 const router = express.Router();
+
+var con = mysql.createConnection({
+    host: "wwwlab.uci.umk.pl",
+    user: "291903_4whad",
+    password: "IH68JU0VR-dp",
+    database: "291903_4wh"
+  });
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -11,6 +19,19 @@ router.get('/:id', (req, res, next) => {
     res.status(200).json({
         message: "requested get for parameter " + req.params.id
     });
+});
+
+router.get('/sqltest/:id', (req, res, next) => {
+
+var sql = 'SELECT * FROM Test WHERE id = ' + mysql.escape(req.params.id);
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.status(200).json({
+        message: "requested get for parameter " + req.params.id,
+        result: result
+    });
+  });
+    
 });
 
 router.post('/', (req, res, next) => {
