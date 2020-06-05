95638eeaaf3269f317d926363bdcdde1edae5cd6
Klarys <44509197+Klarys@users.noreply.github.com>
2020-06-05 18:26:35 +0200

    Added basic login

--- project/node-rest-api/api/routes/user.js
+++ project/node-rest-api/api/routes/user.js
@@ -17,25 +17,56 @@ router.post('/signup', (req, res, next) => {
         if(err) {
             return res.status(500).json({
                 error: err
-            })
+            });
         }
         else
         {
-            var sql = "INSERT INTO `Users` (`Id`, `Login`, `Password`) VALUES (NULL, " + mysql.escape(req.body.login)+ ",'" + hash +"');";
+            var sql = "INSERT INTO `Users` (`Id`, `Login`, `Password`) VALUES (NULL, " + mysql.escape(req.body.login)+ "," + mysql.escape(hash) +");";
 
             con.query(sql, function (err, result) {
                 if (err) {
-                    return res.status(500).json({
-                        error: err.sqlMessage
-                    })
+                    return res.status(409).json({
+                        error: "User can't be created"
+                    });
                 }
                 res.status(201).json({
                     message: "pomyslnie utworzono uzytkownika ",
-                    result: result
+                    result: hash
                 });
               });
         }
     });
+});
+
+router.get('/login', (req, res, next) => {
+
+    con.query('SELECT * FROM `Users` WHERE `Login` = '+mysql.escape(req.body.login), function(err, result) {
+        if (err) {
+            return res.status(404).json({
+                error: "Login failed"
+            });
+        }
+        else
+        {
+            bcrypt.compare(req.body.password, result[0].Password, (error, passwdCheck) => {
+                if(error) {
+                    return res.status(401).json({
+                        error: "Login failed"
+                    });
+                }
+                if(passwdCheck)
+                {
+                    return res.status(200).json({
+                        message: "Login successful"
+                    });
+                }
+                return res.status(401).json({
+                    error: "Login failed"
+                });
+            });
+        }
+    });
+
 });
 
 module.exports = router;
